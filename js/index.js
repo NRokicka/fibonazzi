a8d01f582776ab1d14937ca7edaa960278407c60
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-29 21:57:52 +0000

    add change

--- js/index.js
+++ js/index.js
@@ -1,36 +1,26 @@
 $(function() {
     $('#btn').click(function(e) {
         e.preventDefault();
-        var n = $('#number').val();
+        var num = $('#number').val();
 
-        function fib(n) {
-            return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+        function fib(num) {
+            return num <= 1 ? num : fib(num - 1) + fib(num - 2);
         }
-        var res = fib(n);
+        var res = fib(num);
         $('#result').html(res);
     })
 })
 
+/*$(function() {
+    $('#btn').click(function(e) {
+        e.preventDefault();
+        var num = $('#number').val();
 
-/*
-function fib(n) {
-  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-} 
-
-
-
-
-function fib(n) {
-    var a = 1,
-        b = 1;
-    for (var i = 3; i <= n; i++) {
-        var c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
-}
-
-alert(fib(3)); // 2
-alert(fib(7)); // 13
+        function fib(num) {
+            return num <= 1 ? num : fib(num - 1) + fib(num - 2);
+        }
+        var res = fib(num);
+        $('#result').html(res);
+    })
+})
 */
